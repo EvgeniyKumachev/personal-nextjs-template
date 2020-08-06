b6b5ed93de27ab3a864a83b910a8acd5d5489032
Evgeniy Kumachev <evgeniykumachev@protonmail.ch>
2020-08-06 14:48:13 +0300

    add store-provider

--- src/contexts.tsx
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react'
-
-import { RootStore } from '@app/store'
-
-export const StoreContext = createContext<RootStore | null>(null)
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { observer } from 'mobx-react-lite'
+import { useCreateStore, useProvider } from 'mobx-store-provider'
 import { AppInitialProps, AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
 
 import Layout from '@app/containers/Layout'
-import { StoreContext } from '@app/contexts'
 import { initRootStore } from '@app/store'
 import theme from '@app/theme'
 
@@ -13,16 +13,17 @@ import 'reflect-metadata'
 
 const Application = observer(
   ({ Component, pageProps = {} }: AppProps & AppInitialProps) => {
-    const store = initRootStore(pageProps?.initialState)
+    const store = useCreateStore(() => initRootStore(pageProps?.initialState))
+    const StoreProvider = useProvider()
 
     return (
-      <StoreContext.Provider value={store}>
+      <StoreProvider value={store}>
         <ThemeProvider theme={theme}>
           <Layout>
             <Component {...pageProps} />
           </Layout>
         </ThemeProvider>
-      </StoreContext.Provider>
+      </StoreProvider>
     )
   }
 )
